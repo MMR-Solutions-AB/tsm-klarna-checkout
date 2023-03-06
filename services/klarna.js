3c5832faf7e0222a7c6ab0151fe17e74cad02963
marcuspousette <marcuspousette@gmail.com>
2023-03-06 16:23:10 -0100

    Lektion 6 - Skapa en order #1

--- services/klarna.js
+++ services/klarna.js
@@ -8,7 +8,22 @@ export function getKlarnaAuth() {
 }
 
 // Skapar en order via Klarnas API
-export async function createOrder(product) {}
+export async function createOrder(product) {
+	const path = '/checkout/v3/orders';
+	const auth = getKlarnaAuth();
+
+	const url = process.env.BASE_URL + path;
+	const method = 'POST';
+	const headers = {
+		'Content-Type': 'application/json',
+		Authorization: auth
+	};
+
+	const quantity = 1;
+	const price = product.price * 100;
+	const total_amount = price * quantity;
+	const total_tax_amount = total_amount * 0.2;
+}
 
 // hämtar vår egna skapade order från API
 export async function retrieveOrder(order_id) {}
