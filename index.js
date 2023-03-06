ddfb31135d9b843c78d50193a1c8e4da70d955fa
marcuspousette <marcuspousette@gmail.com>
2023-03-06 15:55:21 -0100

    Lrktion 1 - Skapa endpoints

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+import express from 'express';
+const app = express();
+import { config } from 'dotenv';
+config();
+
+app.get('/', async (req, res) => {
+	const markup = `<h1>Hello world</h1>`;
+	res.send(markup);
+});
+
+app.get('/product/:id', async function (req, res) {
+	const { id } = req.params;
+	const markup = `<h1>Hello ${id}</h1>`;
+	res.send(markup);
+});
+
+app.get('/confirmation', async function (req, res) {
+	const markup = `<h1>Hello confirmation</h1>`;
+	res.send(markup);
+});
+
+app.listen(process.env.PORT);
