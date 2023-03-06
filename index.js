1f440bc044dd80fc23ff434ad2c1ab90506a1b90
marcuspousette <marcuspousette@gmail.com>
2023-03-06 16:07:08 -0100

    Lektion 3 - Visa produkter

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+import { getProducts } from './services/api.js';
 import express from 'express';
 const app = express();
 import { config } from 'dotenv';
 config();
 
 app.get('/', async (req, res) => {
-	const markup = `<h1>Hello world</h1>`;
+	const products = await getProducts();
+	// Lägga till vilken styling som helst nedan
+	const markup = products
+		.map(
+			(p) =>
+				`<a style="display:block;color:black;border:solid black 2px;margin: 20px; padding:10px;" href="/product/${p.id}">
+                    ${p.title} - ${p.price}kr
+                </a>`
+		)
+		.join(' ');
 	res.send(markup);
 });
 
