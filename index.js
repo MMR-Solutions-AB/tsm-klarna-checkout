27184e75c3e6022c4a1ca9ec870a9b5d45ae6424
Marcus Pousette <marcuspousette@gmail.com>
2023-03-16 13:12:31 +0100

    lesson-1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello world');
+import express from 'express';
+const app = express();
+import { config } from 'dotenv';
+config();
+
+app.get('/', async (req, res) => {
+	const markup = `<h1>Hello Klarna</h1>`;
+	res.send(markup);
+});
+
+app.get('/products/:id', async (req, res) => {
+	const { id } = req.params;
+	const markup = `<h1>Hello ${id}</h1>`;
+	res.send(markup);
+});
+
+app.get('/confirmation', async (req, res) => {
+	const markup = `<h1>Hello confiramtion</h1>`;
+	res.send(markup);
+});
+
+app.listen(process.env.PORT);
